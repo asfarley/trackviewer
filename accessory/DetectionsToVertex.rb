5ef92c65ca5896d6cbe2bd4647194af5ad1f7484
asfarley <alexander.s.farley@gmail.com>
2018-03-30 10:19:39 -0600

    Updating detections-log parser to handle JSON.

--- accessory/DetectionsToVertex.rb
+++ accessory/DetectionsToVertex.rb
@@ -1,27 +1,27 @@
+require 'json'
+
 detections_path = ARGV[0]
 vertex_path = File.dirname(detections_path) + "\\vertex.txt"
 
 z = 0
 File.open(vertex_path, 'a') do |file|
 	File.readlines(detections_path).each do |line|
-		detections = line.split(',')
-		detections.each do |d|
-			if d.length > 1
-				properties = d.split(' ')
-				x = properties[1].tr('[]:', '')
-				y = properties[3].tr('[]:', '')
-				r = properties[5].tr('[]:', '')
-				g = properties[7].tr('[]:', '')
-				b = properties[9].tr('[]:', '')
-				cl = properties[11].tr('[]:', '')
-				size = properties[13].tr('[]:', '')
-				width = properties[15].tr('[]:', '')
-				height = properties[17].tr('[]:', '')
+		detectionsJson = JSON.parse(line)
+		detectionsArray = detectionsJson["Measurements"]
+		detectionsArray.each do |d|
+				x = d["X"]
+				y = d["Y"]
+				r = d["Red"]
+				g = d["Green"]
+				b = d["Blue"]
+				cl = d["ObjectClass"]
+				size = d["Size"]
+				width = d["Width"]
+				height = d["Height"]
 				
 				#puts "x:#{x} y:#{y} r:#{r} g:#{g} b:#{b} class:#{cl} size:#{size} width:#{width} height:#{height}"
 				
 				file.puts "#{x} #{y} #{z} #{r} #{g} #{b}"
-			end
 		end
 		z += 1
 	end
